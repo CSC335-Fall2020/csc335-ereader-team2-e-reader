dff9af6ab165980ffd9e97a432a1543ff22002b7
cdillingham <51520844+cdillingham@users.noreply.github.com>
2020-12-05 06:28:48 -0700

    updated code composition + event handling
    Event handling not working currently but I think I am close

--- src/EReaderView.java
+++ src/EReaderView.java
@@ -1,5 +1,5 @@
 /**
- * @author chelseybergmann
+ * @author chelseybergmann, chloed
  * File: EReaderView.java
  * Project: Final Project - E-Reader
  * Purpose: This class is the gui view that displays the reader.
@@ -24,6 +24,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class EReaderView extends Application implements Observer {
@@ -33,37 +34,50 @@ public class EReaderView extends Application implements Observer {
 	public void start(Stage stage) throws Exception {
 		pane = new BorderPane();
 		
-		sideOptions();
-		
-		// Test reading a book.
-		Book book = new Book("Books/salembelle.txt");
-		List<String[]> pages = book.getPages();
-		Label firstPage = new Label();
-		String pageText = "";
-
-			for (String line : pages.get(0)) {
-				pageText += line;
-				pageText += "\n";
-			}
-		
-		firstPage.setText(pageText);
-		pane.setCenter(firstPage);
-		
-		//
-		
+		// initialize the model and controller
 		EReaderModel model = new EReaderModel();
 		EReaderController controller = new EReaderController(model);
 		
 		
+		// Test reading a book.
+		List<String[]> pages = controller.getPages();
+		
+		// create heading 
+		String titleAndAuthor = controller.getTitle() + " by " + controller.getAuthor();
+		Label heading = new Label();
+		heading.setText(titleAndAuthor);
+		pane.setTop(heading);
+		
+		// TO DO: EVENT HANDLER FOR CHANGING FONT / SIZE
+		
+		// set up the pages 
+		setUpPages(pages, "Arial", 12, pane);
+		
+		// display the user's options on the side 
+		// entered info is stored in font and fontSize text fields( index 1 and 3)
+		HBox fonts = sideOptions(pages, pane);
 		
 		// Set and display scene.
-		Scene scene = new Scene(pane, 900, 600);    
-		stage.setTitle("E-Reader GUI View");   
-		stage.setScene(scene);       
-		stage.show(); 
+		setScene(pane, stage); 
 	}
 	
-	private void sideOptions() {
+	private void setUpPages(List<String[]> pages, String fontType, int fontSize, BorderPane pane) {
+		// get the list of pages
+		Label firstPage = new Label();
+		String pageText = "";
+
+		// add an outer loop to create a new screen for each page
+		for (String line : pages.get(0)) {
+				pageText += line;
+				pageText += "\n";
+		}
+		
+		firstPage.setText(pageText);
+		firstPage.setFont(new Font(fontType, fontSize));
+		pane.setCenter(firstPage);
+	}
+	
+	private HBox sideOptions(List<String[]> pages, BorderPane pane) {
 		HBox fonts = new HBox();	
 		Label fontL = new Label("Font:  ");
 		TextField font = new TextField();
@@ -72,9 +86,27 @@ public class EReaderView extends Application implements Observer {
 		TextField fontSize = new TextField();
 		fontSize.setMaxWidth(30);
 		fonts.getChildren().addAll(fontL, font, fontS, fontSize);
+		// entered info is stored in font and fontSize text fields( index 1 and 3)
 		
-		pane.setTop(fonts);
+		font.setOnAction((event) -> {
+			// Use this code to get the user entered font size when filled in
+			String fontSizeStr = fontSize.getText();
+			Integer fontSizeInt = Integer.valueOf(fontSizeStr);
+			String fontType = font.getText();
+			
+			setUpPages(pages, fontType, fontSizeInt, pane);
+			
+		});
 		
+		pane.setLeft(fonts);
+		return fonts;
+	}
+	
+	private void setScene(BorderPane pane, Stage stage) {
+		Scene scene = new Scene(pane, 900, 600);    
+		stage.setTitle("E-Reader GUI View");   
+		stage.setScene(scene);       
+		stage.show(); 
 	}
 	
 	@Override
